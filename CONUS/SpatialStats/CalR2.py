143de1e5193239dbe23b5e229dcda56c9c2f4186
Yanlan Liu <yanlan@sh01-ln03.stanford.edu>
2020-06-21 16:51:50 -0700

    Updated accuracy metrics

--- CONUS/SpatialStats/CalR2.py
+++ CONUS/SpatialStats/CalR2.py
@@ -8,14 +8,20 @@ from Utilities import MovAvg
 import pickle
 import time
 
-parentpath = '/scratch/users/yanlan/' 
-arrayid = int(os.environ['SLURM_ARRAY_TASK_ID']) # 0-14
-nsites_per_id = 1000
+# parentpath = '/scratch/users/yanlan/' 
+# arrayid = int(os.environ['SLURM_ARRAY_TASK_ID']) # 0-14
+# nsites_per_id = 1000
 
 
+<<<<<<< HEAD
 #parentpath = '/Volumes/ELEMENTS/VOD_hydraulics/'
 #arrayid = 83
 #nsites_per_id = 1
+=======
+parentpath = '/Volumes/ELEMENTS/VOD_hydraulics/'
+arrayid = 83
+nsites_per_id = 1
+>>>>>>> 0eb9435629b94bfcef5e52d947f15177b90aa985
 
 versionpath = parentpath + 'Retrieval_0510/'
 inpath = parentpath+'Input/'
@@ -71,4 +77,7 @@ print(f"Running time (1000 sites): {toc-tic:0.4f} seconds")
 r2name = r2path+'R2_'+str(arrayid)+'_1E3.pkl'
 with open(r2name, 'wb') as f: 
     pickle.dump([R2, RMSE,corr,MissingList], f)
+<<<<<<< HEAD
 
+=======
+>>>>>>> 0eb9435629b94bfcef5e52d947f15177b90aa985
--- CONUS/TroubleShooting/LoadForwardTS.py
+++ CONUS/TroubleShooting/LoadForwardTS.py
@@ -15,6 +15,7 @@ from newfun import readCLM
 from Utilities import MovAvg
 import pickle
 import matplotlib.pyplot as plt
+from scipy.stats import norm
 # parentpath = '/scratch/users/yanlan/' 
 # arrayid = int(os.environ['SLURM_ARRAY_TASK_ID']) # 0-14
 nsites_per_id = 1000
@@ -31,6 +32,7 @@ outpath = versionpath+'Output/'
 #r2path = parentpath+'R2_test/'
 forwardpath = versionpath+'Forward/'
 traitpath = versionpath+'Traits/'
+likpath = versionpath+'Loglik/'
 
 
 SiteInfo = pd.read_csv('../Utilities/SiteInfo_US_full.csv')
@@ -38,9 +40,12 @@ MODE = 'AM_PM_ET_'
 
 trb = pd.read_csv('trb_list.csv')
 
-
+A0 = SiteInfo[['row','col']]
+A1 = trb[['row','col']]
+A2 = A1.merge(A0,how='outer',indicator=True).loc[lambda x : x['_merge']=='right_only']
+ 
 #%%
-tid = 30
+tid = 1000
 tmp = trb.iloc[tid]
 fid = np.where((SiteInfo['row']==tmp['row']) & (SiteInfo['col']==tmp['col']))[0][0]
 arrayid = int(fid/nsites_per_id)
@@ -56,20 +61,25 @@ if os.path.isfile(forwardname):
         SVOD, SET, SPSIL,SPOPT = pickle.load(f)
 
 plt.figure()
+plt.subplot(211)
 plt.plot(ET,'-r')
 plt.plot(np.nanmean(SET,axis=0),'-',color='navy')
-
-plt.figure()
+plt.subplot(212)
 plt.plot(VOD,'-r')
 plt.plot(np.nanmean(SVOD,axis=0),'-',color='navy')
 
 # Next, calculate logliklihood of ET and VOD. See if VOD smashes ET. If so, play weith traits
-#%%
-traitname = traitpath+'Traits_'+str(arrayid)+'_1E3.pkl'
 
-with open(traitname, 'rb') as f: 
-    Val_25, Val_50, Val_75, MissingList = pickle.load(f)
-        
-#%%
-Val_50.shape
+likname = likpath+'Loglik_'+str(arrayid)+'_1E3.pkl'
+
+with open(likname, 'rb') as f: 
+    Lik_25, Lik_50, Lik_75, MissingList = pickle.load(f)
+     
+sigma_et, sigma_vod, loglik = Lik_25[fid-arrayid*nsites_per_id,:]
+valid_vod = ~np.isnan(VOD_ma)
+valid_et = ~np.isnan(ET)
+loglik_vod = np.nanmean(norm.logpdf(VOD_ma,np.nanmean(SVOD,axis=0),sigma_vod))
+loglik_et = np.nanmean(norm.logpdf(ET,np.nanmean(SET,axis=0),sigma_et))
+
+print([loglik_vod,loglik_et])
 
