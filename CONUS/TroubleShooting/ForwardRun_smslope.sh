63639077e39900b88da71bc7c67e12746ff48de1
Yanlan Liu <yanlan@sh01-ln04.stanford.edu>
2020-06-28 11:19:57 -0700

    forward runs of 3 retrievals

---
+++ CONUS/TroubleShooting/ForwardRun_smslope.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=fw_smslope
+#SBATCH --output=JobInfo/%x_%a.out
+#SBATCH --error=JobInfo/%x_%a.err
+#SBATCH --array=0-99
+#SBATCH --ntasks=1
+#SBATCH -p konings,owners,normal
+#SBATCH --time=1:00:00
+#SBATCH --mem-per-cpu=2000
+
+######################
+# Begin work section #
+######################
+
+# Print this sub-job's task ID
+echo "GRID: " $SLURM_ARRAY_TASK_ID >> ForwardRun_smslope.out
+python ForwardRun_smslope.py
+~
