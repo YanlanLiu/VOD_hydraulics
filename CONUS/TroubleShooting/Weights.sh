cc82dd44fe37231a167043c3d1c07ba94dfd9008
Yanlan Liu <liuyanlan10@gmail.com>
2020-06-27 22:53:49 -0700

    weight by catagory

---
+++ CONUS/TroubleShooting/Weights.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=wt
+#SBATCH --output=JobInfo/%x_%a.out
+#SBATCH --error=JobInfo/%x_%a.err
+#SBATCH --array=0-99
+#SBATCH --ntasks=1
+#SBATCH -p konings,owners,normal
+#SBATCH --time=2:00:00
+#SBATCH --mem-per-cpu=2000
+
+######################
+# Begin work section #
+######################
+
+# Print this sub-job's task ID
+echo "GRID: " $SLURM_ARRAY_TASK_ID >> Weights.out
+python Weights.py 
+
+
