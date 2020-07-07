fb0a4e6da0921220ad939e84159648253dd9324b
Yanlan Liu <yanlan@sh01-ln04.stanford.edu>
2020-07-07 14:44:17 -0700

    added forward runs for ETwd and ISO

---
+++ CONUS/TroubleShooting/ForwardRun_MC_SM.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=fw_sm
+#SBATCH --output=JobInfo/%x_%a.out
+#SBATCH --error=JobInfo/%x_%a.err
+#SBATCH --array=0-49
+#SBATCH --ntasks=1
+#SBATCH -p konings,owners,normal
+#SBATCH --time=1:00:00
+#SBATCH --mem-per-cpu=2000
+
+######################
+# Begin work section #
+######################
+
+# Print this sub-job's task ID
+echo "GRID: " $SLURM_ARRAY_TASK_ID >> ForwardRun_MC_SM.out
+python ForwardRun_MC_SM.py
+~
