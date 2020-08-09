217d9a0417f7e4c7c51189ff5d3a7a33dca5798b
Yanlan Liu <yanlan@sh01-ln01.stanford.edu>
2020-08-09 11:09:51 -0700

    sherlock version

---
+++ CONUS/OSSE/OSSE_dET2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=dET2
+#SBATCH --output=JobInfo/%x_%a.out
+#SBATCH --error=JobInfo/%x_%a.err
+#SBATCH --array=0-105
+#SBATCH --ntasks=1
+#SBATCH -p konings,owners,normal
+#SBATCH --time=6:00:00
+#SBATCH --mem-per-cpu=2000
+
+######################
+# Begin work section #
+######################
+
+# Print this sub-job's task ID
+echo "GRID: " $SLURM_ARRAY_TASK_ID >> OSSE_dET2.out
+python OSSE_dET.py 2
+~
