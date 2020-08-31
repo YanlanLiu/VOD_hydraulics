4256cb8e5d9fcd64e73895e7ab45350f0dc1fc4e
Yanlan Liu <liuyanlan10@gmail.com>
2020-08-30 19:04:18 -0700

    forward by pft and cluster

--- GLOBAL/PCA.py
+++ GLOBAL/PCA.py
@@ -83,7 +83,10 @@ def plotmap(df,varname,vmin=0,vmax=1,cmap=mycmap):
     return 0
 #%%
 df_para = pd.DataFrame(Collection_PARA,columns=varnames+['a','b','c'])
-df_para['row'] = SiteInfo['row'];df_para['col'] = SiteInfo['col']
+df_para['row'] = SiteInfo['row'];df_para['col'] = SiteInfo['col']; df_para['IGBP'] = SiteInfo['IGBP']
+df_para['Root depth'] = SiteInfo['Root depth']; df_para['Soil texture'] = SiteInfo['Soil texture']
+df_para['Vcmax25'] = SiteInfo['Vcmax25']
+
 df_para = df_para.dropna()
 plotmap(df_para,'psi50X',vmin=0,vmax=7,cmap='RdYlBu_r')
 
@@ -92,50 +95,37 @@ plotmap(df_para,'psi50X',vmin=0,vmax=7,cmap='RdYlBu_r')
 #%% PCA
 from numpy import linalg
 Y = np.array(df_para[varnames[:5]])
+Y[:,2] = -Y[:,2]
 Y = Y[np.where(~np.isnan(Y[:,1]))[0],:]
 Y[:,1] = Y[:,1]*Y[:,2]
 # n, p = Y.shape
 # C = (np.eye(n)-np.ones([n,n])/n)
 # Yc = np.dot(C,Y)
-Yc = (Y-np.nanmean(Y,axis=0))/np.nanstd(Y,axis=0)
+
+Ymean = np.nanmean(Y,axis=0)
+Ystd = np.nanstd(Y,axis=0)
+Yc = (Y-Ymean)/Ystd
 S = np.dot(Yc.T,Yc)
-
-
 w, v = linalg.eig(S)
 sw = w[np.argsort(w)[::-1]]
 sv = v[:,np.argsort(w)[::-1]]
 f1 = np.dot(Yc,sv[:,0])
 f2 = np.dot(Yc,sv[:,1])
-plt.figure()
-plt.plot(f1,f2,'o')
-
+# plt.figure()
+# plt.plot(f1,f2,'o')
 df_pca = df_para[['row','col']][:len(f1)]
 df_pca['f1'] = f1
 df_pca['f2'] = f2
 
-#%%
-import seaborn as sns; sns.set(style="ticks", color_codes=True,font_scale=1.75)
-plotmap(df_pca,'f1',vmin=-2,vmax=2,cmap='RdYlBu_r')
-plotmap(df_pca,'f2',vmin=-2,vmax=2,cmap='RdYlBu_r')
-
-# heatmap1_data = pd.pivot_table(df_pca, values='f2', index='row', columns='col')
-# mycmap = sns.cubehelix_palette(rot=-.63, as_cmap=True)
-
-# plt.imshow(heatmap1_data,cmap='RdBu_r',vmin=-2,vmax=2);plt.colorbar()
-# plt.xticks([]);plt.yticks([])
-# plt.title('PC2')
-#%%
-IGBPlist = ['NA','ENF','EBF','DNF','DBF','MF','CSB','OSB',
-            'WSV','SAV','GRA','CRO/URB','CRO','URB','GRA','SNW','NA','NA','NA']
-
-plotmap(SiteInfo,'IGBP',vmin=0,vmax=13,cmap=mycmap)
-
-#%%
+# import seaborn as sns; sns.set(style="ticks", color_codes=True,font_scale=1.75)
+# plotmap(df_pca,'f1',vmin=-2,vmax=2,cmap='RdYlBu_r')
+# plotmap(df_pca,'f2',vmin=-2,vmax=2,cmap='RdYlBu_r')
 plt.plot(np.cumsum(sw)/np.sum(sw)*100,'^-k')
 plt.ylim([0,110])
 plt.xlabel('number of PCs')
 plt.ylabel('% of var. explained')
-#%%
+
+plt.figure()
 xlim = [-0.8,0.8]
 plt.plot(sv[:,0],sv[:,1],'ok')
 for i in range(5):
@@ -147,34 +137,111 @@ plt.ylim(xlim)
 plt.xlabel('PC1')
 plt.ylabel('PC2')
 
+
+IGBPlist = ['NA','ENF','EBF','DNF','DBF','MF','CSB','OSB',
+            'WSV','SAV','GRA','CRO/URB','CRO','URB','GRA','SNW','NA','NA','NA']
+
+plotmap(df_para,'IGBP',vmin=0,vmax=13,cmap=mycmap)
+
+#%%
+import pickle
+#% variance within each PFT
+IGBParray = np.array(df_para['IGBP'])
+IGBPunique = np.unique(IGBParray)
+count = [sum(df_para['IGBP']==itm) for itm in IGBPunique]
+SST = 0
+PFTcenter = []
+for itm in IGBPunique:
+    Yn = Yc[IGBParray==itm,:]
+    mun = np.mean(Yn,axis=0)
+    PFTcenter.append(mun)
+    SST = SST+np.sum((Yn-mun)**2)
+Vw_pft =  SST/Yc.shape[0]
+
+PFTcenter = np.array(PFTcenter)
+Vb_pft = np.sum((PFTcenter-np.mean(PFTcenter,axis=0))**2)/len(IGBPunique)
+
+# with open('PFT_center.pkl', 'wb') as f:
+#     pickle.dump((IGBPunique,PFTcenter),f)
+    
 #%% K-means clustering
 from sklearn.cluster import KMeans
+from scipy.stats import multivariate_normal
 import numpy as np
 # X = np.array([[1, 2], [1, 4], [1, 0],
 #               [10, 2], [10, 4], [10, 0]])
-NCluster = 9
-kmeans = KMeans(n_clusters=NCluster, random_state=0).fit(Yc)
-kmeans.labels_
-kmeans.cluster_centers_
 
+def fit_kmeans(Yc,n_clusters):
+    # n_clusters = 4
+    R,K = Yc.shape
+    
+    kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(Yc)
+    
+    SST = 0
+    for n in range(n_clusters):
+        Yn = Yc[kmeans.labels_==n,:]
+        mun = np.mean(Yn,axis=0)
+        SST = SST+np.sum((Yn-mun)**2)
+        # Va = Va + np.sum((Yn-mun)**2)
+    
+    sigma2 = SST/K/(R-K)
+    
+    Vall = np.sum((Yc-np.mean(Yc,axis=0))**2)/R
+    Vb = np.sum((kmeans.cluster_centers_-np.mean(kmeans.cluster_centers_,axis=0))**2)/n_clusters
+    Vw = SST/R
+    
+    loglik = 0
+    for n in range(n_clusters):
+        Yn = Yc[kmeans.labels_==n,:]
+        Rn = Yn.shape[0]
+        mun = np.mean(Yn,axis=0)
+        # loglik = loglik+np.sum(np.log(multivariate_normal.pdf(Yn, mean=mun, cov=sigma2*np.eye(K))))+Rn*np.log(Rn/R)
+        loglik = loglik+np.sum(np.log(multivariate_normal.pdf(Yn, mean=mun, cov=sigma2*np.eye(K))*Rn/R))
+        
+    BIC = (K*n_clusters+1)*np.log(R)-2*loglik
+    # AIC = 2*(K*n_clusters+1)-2*loglik
+    return kmeans.labels_, kmeans.cluster_centers_,Vall,Vb,Vw,BIC#, AIC
+
+n_clusters = 4
+klabel, kcenter, Vall,Vb,Vw, BIC = fit_kmeans(Yc,n_clusters)
+print(Vb/Vall,Vw/Vb,BIC)
+sorted_idx = list(np.argsort(kcenter[:,2]))
+kcenter_s = kcenter[sorted_idx,:]
+klabel_s = np.array([sorted_idx.index(i) for i in klabel])
+df_para['clusters_4'] = klabel_s
+# with open('kcenter_4.pkl', 'wb') as f:
+#     pickle.dump(kcenter_s,f)
+#%%
+# Add a figure showing decay of BIC and Vw/Vb as n_clusters increases
+
+n_clusters = 12
+klabel, kcenter, Vall,Vb,Vw, BIC = fit_kmeans(Yc,n_clusters)
+print(Vb/Vall,Vw/Vb,BIC)
+sorted_idx = list(np.argsort(kcenter[:,2]))
+kcenter_s = kcenter[sorted_idx,:]
+klabel_s = np.array([sorted_idx.index(i) for i in klabel])
+df_para['clusters_12'] = klabel_s
+with open('kcenter_12.pkl', 'wb') as f:
+    pickle.dump(kcenter_s,f)
+df_para.to_csv('SiteInfo_clusters.csv')
 
 #%%
-import seaborn as sns; sns.set(style="ticks", color_codes=True,font_scale=1.15)
-
 from mpl_toolkits.mplot3d import Axes3D
 # colors = [sns.color_palette("BrBG_r",9)[i] for i in [1,3,5,7]]
-colors = [sns.color_palette("BrBG_r",9)[i] for i in np.arange(9)]
+# cc = [sns.color_palette("BrBG",n_clusters)[i] for i in np.arange(n_clusters)]
+cc = [sns.color_palette("hls", n_clusters)[i] for i in np.arange(n_clusters)]
 
 fig = plt.figure(figsize=(6,6))
 ax = fig.add_subplot(111, projection='3d')
-for i in range(NCluster):
+for i in range(n_clusters):
     # ax.scatter(kmeans.cluster_centers_[i,0],kmeans.cluster_centers_[i,1],kmeans.cluster_centers_[i,4],'o',s=50,label='C'+str(i))
-    ax.scatter(kmeans.cluster_centers_[i,0],kmeans.cluster_centers_[i,2],kmeans.cluster_centers_[i,3],'o',color=colors[i],edgecolors='k',s=50,label='C'+str(i))
-ax.set_xlabel('g1')
-ax.set_ylabel('p50')
-ax.set_zlabel('gpmax')
+    ax.scatter(kcenter_s[i,2],kcenter_s[i,3],kcenter_s[i,0],'o',color=cc[i],edgecolors='k',s=50,label='C'+str(i))
+ax.set_xlabel('p50')
+ax.set_ylabel('gpmax')
+ax.set_zlabel('g1')
 plt.legend(bbox_to_anchor=(1.35,1.05))
-# for i in range(4):
+
+# for i in range(4):|
 #     ax.text(kmeans.cluster_centers_[i,0],kmeans.cluster_centers_[i,2],kmeans.cluster_centers_[:,3],'s')
 
 #%%
@@ -182,15 +249,15 @@ import seaborn as sns; sns.set(style="ticks", color_codes=True,font_scale=1.75)
 
 from matplotlib import colors
 df_km =  df_para[['row','col']]
-df_km['km'] = kmeans.labels_
+df_km['clusters'] = klabel_s
 # sns.color_palette("Set2")[3:8]
 # cmap0 = colors.ListedColormap([sns.color_palette("colorblind")[i] for i in [0,2,4,6]])
 # cmap0 = colors.ListedColormap([sns.color_palette("BrBG_r",9)[i] for i in [1,3,5,7]])
-cmap0 = colors.ListedColormap([sns.color_palette("BrBG_r",9)[i] for i in np.arange(9)])
+cmap0 = colors.ListedColormap(cc)
+plotmap(df_km,'clusters',vmin=0,vmax=n_clusters,cmap=cmap0)
 
-plotmap(df_km,'km',vmin=0,vmax=NCluster,cmap=cmap0)
 
-
+#%%
 
 #%%
 
